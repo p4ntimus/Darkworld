15febd3f4165c18b6be9dd50d85b527afddcdd6d
p4ntimus <p4ntimus@gmail.com>
2026-05-21 00:12:56 +0200

    Update chat.js

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,1 +1,40 @@
+import { api } from './api.js';
+
+export function initChat() {
+    const sendBtn = document.getElementById("sendBtn");
+    const msgBox = document.getElementById("messages");
+
+    if (!sendBtn) return;
+
+    sendBtn.addEventListener("click", sendMessage);
+
+    setInterval(loadMessages, 2000);
+}
+
+async function sendMessage() {
+    const name = localStorage.getItem("dw_user");
+    const text = document.getElementById("text").value.trim();
+
+    if (!text) return;
+
+    await api("send", { name, text });
+    document.getElementById("text").value = "";
+}
+
+async function loadMessages() {
+    const msgBox = document.getElementById("messages");
+    const msgs = await api("messages");
+
+    msgBox.innerHTML = "";
+
+    msgs.forEach(m => {
+        msgBox.innerHTML += `
+            <div class="msg">
+                <span class="name">${m.name}:</span> ${m.text}
+            </div>
+        `;
+    });
+
+    msgBox.scrollTop = msgBox.scrollHeight;
+}
 
