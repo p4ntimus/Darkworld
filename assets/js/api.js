72207ce98c5c4e4f94082697ed17373c88fdd783
p4ntimus <p4ntimus@gmail.com>
2026-05-21 00:12:19 +0200

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,14 @@
+const API = "https://darkworld.infinityfreeapp.com/server.php";
+
+export async function api(action, data = {}) {
+    const form = new FormData();
+    for (const key in data) form.append(key, data[key]);
+
+    const res = await fetch(`${API}?action=${action}`, {
+        method: "POST",
+        body: form
+    });
+
+    return res.json();
+}
 
