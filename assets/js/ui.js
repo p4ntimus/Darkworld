6ef2a41ac03e01298d6973da572293d8828d4518
p4ntimus <p4ntimus@gmail.com>
2026-05-21 00:13:09 +0200

    Update ui.js

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,1 +1,12 @@
+export function fadeIn(el) {
+    el.style.opacity = 0;
+    el.style.display = "block";
+
+    let o = 0;
+    const timer = setInterval(() => {
+        o += 0.05;
+        el.style.opacity = o;
+        if (o >= 1) clearInterval(timer);
+    }, 20);
+}
 
