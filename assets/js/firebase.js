2773ed25b0a5b28d9bb64836592d7946db91d749
p4ntimus <p4ntimus@gmail.com>
2026-05-21 12:37:30 +0200

    Create firebase.js

---
+++ assets/js/firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCxfnauuLeiCTdMja-I1URsWIjg-boXRdo",
+    authDomain: "darkworld-39289.firebaseapp.com",
+    projectId: "darkworld-39289",
+    storageBucket: "darkworld-39289.firebasestorage.app",
+    messagingSenderId: "418903434311",
+    appId: "1:418903434311:web:9f56fa96471b6548427d62"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
