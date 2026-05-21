8e1b19bea30d1fcf23dae76c3a76a3e1f61427d7
p4ntimus <p4ntimus@gmail.com>
2026-05-21 12:47:14 +0200

    Update world.js

--- assets/js/world.js
+++ assets/js/world.js
@@ -1,2 +1,14 @@
-const user = localStorage.getItem("username");
-document.getElementById("hud").innerText = `${user} – Level 1 – 100 HP – 50 Gold`;
+import { db } from "./firebase.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+
+const uid = localStorage.getItem("uid");
+
+async function loadPlayer() {
+    const snap = await getDoc(doc(db, "players", uid));
+    const p = snap.data();
+
+    document.getElementById("hud").innerText =
+        `${p.name} – Level ${p.level} – ${p.hp} HP – ${p.gold} Gold`;
+}
+
+loadPlayer();
