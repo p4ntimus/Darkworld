ea1b6cf15f68dadbedb2b73afcfa7e0d89e8c074
p4ntimus <p4ntimus@gmail.com>
2026-05-21 12:46:36 +0200

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,10 +1,8 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+  import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+  import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+  import { getDatabase, ref, push, onChildAdded } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
 
-  // Your web app's Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyCxfnauuLeiCTdMja-I1URsWIjg-boXRdo",
     authDomain: "darkworld-39289.firebaseapp.com",
@@ -15,5 +13,7 @@
   };
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+  export const app = initializeApp(firebaseConfig);
+  export const auth = getAuth(app);
+  export const db = getFirestore(app);
+  export const rtdb = getDatabase(app);
